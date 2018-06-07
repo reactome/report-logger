8a37c574fe4b54c08a866d804cb76cf5fca1b445
gviteri <gviteri@ebi.ac.uk>
2018-06-07 23:33:39 +0100

    adding logo as base64 in the template

--- src/main/java/org/reactome/server/controller/DigesterScheduler.java
+++ src/main/java/org/reactome/server/controller/DigesterScheduler.java
@@ -6,12 +6,16 @@ import org.reactome.server.util.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,16 +51,27 @@ public class DigesterScheduler {
         return "redirect:/lastmonth";
     }
 
-    @Scheduled(cron = "0 7 1 * *") // every Monday at 07AM
+    //@Scheduled(cron = "0 7 1 * *") // every Monday at 07AM
     @GetMapping(value="/lastmonth")
     @ResponseStatus(HttpStatus.OK)
-    public String monthlyReport() {
+    public String monthlyReport() throws IOException {
 
         Mail mail = new Mail("definefrom@reactome.org", mailTo, "[Search] Monthly report", "search-target.ftl");
         Map<String, Object> model = new HashMap<>();
         model.put("title", "Monthly report");
         model.put("targetSummary", targetDigesterService.findLastMonthTargetsByDateTerm());
         model.put("targetsByIp", targetDigesterService.findLastMonthTargetsByDateTermAndIp());
+
+        //Get file from resources folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource("templates/logo.png");
+        if (resource != null) {
+            byte[] data = Files.readAllBytes(new File(resource.getFile()).toPath());
+            byte[] encoded = Base64.getEncoder().encode(data);
+            String imgDataAsBase64 = new String(encoded);
+            String imgAsBase64 = "data:image/png;base64," + imgDataAsBase64;
+            model.put("logo", imgAsBase64);
+        }
         mail.setModel(model);
 
         mailService.sendEmail(mail);
