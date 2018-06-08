29827fed97ecbb0d61f27f28e35edf06be085afc
gviteri <gviteri@ebi.ac.uk>
2018-06-08 14:57:05 +0100

    adding hostname and inline-css to be compatible with gmail

--- src/main/java/org/reactome/server/controller/DigesterScheduler.java
+++ src/main/java/org/reactome/server/controller/DigesterScheduler.java
@@ -14,11 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.Base64;
+import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,14 +29,18 @@ public class DigesterScheduler {
     private TargetDigesterService targetDigesterService;
     private SearchDigesterService searchDigesterService;
     private MailService mailService;
+    private String hostname;
 
-    public DigesterScheduler(@Value("${mail.report}") String mailTo) {
+    public DigesterScheduler(@Value("${mail.report}") String mailTo,
+                             @Value("${mail.report.hostname}") String hostname) {
         this.mailTo = mailTo;
+        this.hostname = hostname;
     }
 
     @Scheduled(cron = "0 12 * * SAT") // every Monday at 07AM
     @ResponseStatus(HttpStatus.OK)
     public void weeklyReport() {
+        if (!sendReport()) return;
         Mail mail = new Mail("Reactome Report <noreply@reactome.org>", mailTo, "[Search] Weekly report", "search-target.ftl");
         Map<String, Object> model = new HashMap<>();
         model.put("title", "Weekly report");
@@ -48,7 +48,7 @@ public class DigesterScheduler {
         model.put("targetsByIp", targetDigesterService.findLastWeekTargetsByDateTermAndIp());
         model.put("searchSummary", searchDigesterService.findLastWeekSearchesByDateTerm());
         model.put("searchByIp", searchDigesterService.findLastWeekSearchesByDateTermAndIp());
-        model.put("logo", getLogoAsBase64());
+        model.put("logo", getLogo());
         mail.setModel(model);
 
         mailService.sendEmail(mail);
@@ -57,6 +57,7 @@ public class DigesterScheduler {
     @Scheduled(cron = "0 1 1 * *") // every Monday at 01AM
     @ResponseStatus(HttpStatus.OK)
     public void monthlyReport() {
+        if (!sendReport()) return;
         Mail mail = new Mail("Reactome Report  <noreply@reactome.org>", mailTo, "[Search] Monthly report", "search-target.ftl");
         Map<String, Object> model = new HashMap<>();
         model.put("title", "Monthly report");
@@ -64,7 +65,7 @@ public class DigesterScheduler {
         model.put("targetsByIp", targetDigesterService.findLastMonthTargetsByDateTermAndIp());
         model.put("searchSummary", searchDigesterService.findLastMonthSearchesByDateTerm());
         model.put("searchByIp", searchDigesterService.findLastMonthSearchesByDateTermAndIp());
-        model.put("logo", getLogoAsBase64());
+        model.put("logo", getLogo());
         mail.setModel(model);
 
         mailService.sendEmail(mail);
@@ -81,7 +82,7 @@ public class DigesterScheduler {
         model.put("targetsByIp", targetDigesterService.findLastWeekTargetsByDateTermAndIp());
         model.put("searchSummary", searchDigesterService.findLastWeekSearchesByDateTerm());
         model.put("searchByIp", searchDigesterService.findLastWeekSearchesByDateTermAndIp());
-        model.put("logo", getLogoAsBase64());
+        model.put("logo", getLogo());
         mail.setModel(model);
         mailService.sendEmail(mail);
     }
@@ -97,7 +98,7 @@ public class DigesterScheduler {
         model.put("targetsByIp", targetDigesterService.findLastMonthTargetsByDateTermAndIp());
         model.put("searchSummary", searchDigesterService.findLastMonthSearchesByDateTerm());
         model.put("searchByIp", searchDigesterService.findLastMonthSearchesByDateTermAndIp());
-        model.put("logo", getLogoAsBase64());
+        model.put("logo", getLogo());
         mail.setModel(model);
         mailService.sendEmail(mail);
     }
@@ -121,21 +122,15 @@ public class DigesterScheduler {
      * Get logo stored in resources and encode to Base64. If the logo ins't found then get the logo
      * directly from the web site.
      */
-    private String getLogoAsBase64() {
-        String imgAsBase64 = "https://reactome.org/templates/favourite/images/logo/logo.png";
-        try {
-            ClassLoader classLoader = getClass().getClassLoader();
-            URL resource = classLoader.getResource("templates/logo.png");
-            if (resource != null) {
-                byte[] data = Files.readAllBytes(new File(resource.getFile()).toPath());
-                byte[] encoded = Base64.getEncoder().encode(data);
-                String imgDataAsBase64 = new String(encoded);
-                imgAsBase64 = "data:image/png;base64," + imgDataAsBase64;
-            }
-        } catch (IOException e) {
-            // nothing here
-        }
-        return imgAsBase64;
+    private String getLogo() {
+        return "https://reactome.org/templates/favourite/images/logo/logo.png";
+    }
 
+    private boolean sendReport() {
+        try {
+            return hostname.equalsIgnoreCase(InetAddress.getLocalHost().getHostName());
+        } catch (Throwable t) {
+            return false;
+        }
     }
 }
