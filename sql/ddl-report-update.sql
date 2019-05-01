04ea4e2eff1a27938c16d6c5d909fc7ea598ed96
gviteri <gviteri@ebi.ac.uk>
2019-05-01 11:07:15 +0100

    adding orcid_token

--- sql/ddl-report-update.sql
+++ sql/ddl-report-update.sql
@@ -6,3 +6,15 @@ create table orcid_claim (
      last_modified datetime,
      primary key (orcid, st_id)
 ) engine=InnoDB;
+
+
+create table orcid_token (
+     orcid varchar(20) not null,
+     access_token varchar(100) not null,
+     expires_in bigint,
+     name varchar(255) not null,
+     refresh_token varchar(100) not null,
+     scope varchar(100),
+     token_type varchar(50),
+     primary key (orcid)
+) engine=InnoDB;
