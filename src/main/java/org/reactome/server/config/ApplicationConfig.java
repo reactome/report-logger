96a65fb600c6891aea9ee80260308b4dbc7881d9
Guilherme Viteri <gviteri@ebi.ac.uk>
2021-07-06 12:08:58 +0100

    Clean up

--- src/main/java/org/reactome/server/config/ApplicationConfig.java
+++ src/main/java/org/reactome/server/config/ApplicationConfig.java
@@ -12,36 +12,4 @@ import org.springframework.context.annotation.PropertySource;
 @Import(value = CustomSecurityConfiguration.class)
 class ApplicationConfig {
 
-//    @Bean
-//    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-//        return new PropertySourcesPlaceholderConfigurer();
-//    }
-
-//    @Bean
-//    public JavaMailSender getMailSender(@Value("${mail.host}") String host,
-//                                        @Value("${mail.port}") String port,
-//                                        @Value("${mail.username}") String username,
-//                                        @Value("${mail.password}") String password,
-//                                        @Value("${mail.enable.auth}") String enableAuth) {
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost(host);
-//        mailSender.setPort(Integer.parseInt(port));
-//        mailSender.setUsername(username);
-//        mailSender.setPassword(password);
-//
-//        Properties javaMailProperties = new Properties();
-//        javaMailProperties.put("mail.transport.protocol", "smtp");
-//        javaMailProperties.put("mail.debug", "false");
-//        javaMailProperties.put("mail.smtp.ssl.trust", "*");
-//
-//        mailSender.setJavaMailProperties(javaMailProperties);
-//        return mailSender;
-//    }
-//
-//    @Bean
-//    public FreeMarkerConfigurationFactoryBean getFreeMarkerConfiguration() {
-//        FreeMarkerConfigurationFactoryBean bean = new FreeMarkerConfigurationFactoryBean();
-//        bean.setTemplateLoaderPath("classpath:/templates/");
-//        return bean;
-//    }
 }
